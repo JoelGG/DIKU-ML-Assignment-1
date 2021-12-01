dd8ed70232207097b60f9e087d1378740642ff29
JoelGG <joel.grimmer@gmail.com>
2021-12-01 13:46:47 +0100

    tidied up q3

--- Code/question_3_inequalities.py
+++ Code/question_3_inequalities.py
@@ -4,34 +4,32 @@ import math
 
 
 def main():
-    q2a(0.5, 0.25, 20, 0.5, 1.05, 0.05)
-    q2b()
+    q2(0.5, 20, 0.5, 1.05, 0.05)  # bias of 0.5, 20 repetitions of coin flip
+    q2(0.1, 20, 0.1, 1.05, 0.05)  # bias of 0.1, 20 repetitions of coin flip
 
 
-def q2a(ex, vx, n, a_start, a_stop, a_step):
-    ex = 0.5
-    vx = 0.25
-    n = 20
-
-    def emfreq():
-        y_vals = (np.random.binomial(20, 0.5, 1000000) / 20)
-        alphas = np.arange(a_start, a_stop, a_step)
-        ps = np.zeros((len(alphas)))
-        for n in range(0, len(alphas)):
-            alpha = alphas[n]
-            ps[n] = (y_vals >= alpha).sum() / 1000000
-        return ps
+def q2(ex, n, a_start, a_stop, a_step, reps=1000000):
+    vx = ex * (1 - ex)
 
     def markov(a): return ex / a
     def cheb(a): return vx / (n * ((a - ex) ** 2))
     def hoff(a): return 1 / (math.e ** (2 * n * ((a - ex) ** 2)))
+
+    def emfreq():
+        y_vals = (np.random.binomial(n, ex, reps) / n)
+        alphas = np.arange(a_start, a_stop, a_step)
+        ps = np.zeros((len(alphas)))
+        for i in range(0, len(alphas)):
+            alpha = alphas[i]
+            ps[i] = (y_vals >= alpha).sum() / reps
+        return ps
 
     alpha = np.arange(a_start, a_stop, a_step)
 
-    freq_y = emfreq()
     markov_y = np.array(list(map(markov, alpha)))
     cheb_y = np.array(list(map(cheb, alpha)))
     hoff_y = np.array(list(map(hoff, alpha)))
+    freq_y = emfreq()
 
     plt.plot(alpha, freq_y, label="Empirical frequency")
     plt.plot(alpha, markov_y, label="Markov")
@@ -39,50 +37,12 @@ def q2a(ex, vx, n, a_start, a_stop, a_step):
     plt.plot(alpha, hoff_y, label="Hoeffding")
     plt.ylabel(r"$\mathbb{P}(\frac{1}{20}\sum^{20}_{i = 1}{X_i} \geq \alpha)$")
     plt.xlabel("\u03B1")
-    plt.title("Bias of 0.5")
+    plt.title(f"Bias of {ex}")
     plt.ylim(0, 1)
-    plt.xlim(0.5, 1)
+    plt.xlim(a_start, a_stop - a_step)
     plt.legend()
-    plt.savefig("./Plots/plot_q3_1", dpi=200)
+    plt.savefig(str(f"./Plots/plot_q3_{int(20 * ex)}.png"), dpi=200)
     plt.cla()
-
-
-def q2b():
-    ex = 0.1
-    vx = 0.09
-    n = 20
-
-    def emfreq():
-        y_vals = (np.random.binomial(20, 0.1, 1000000) / 20)
-        alphas = np.arange(0.1, 1.05, 0.05)
-        ps = np.zeros((19))
-        for n in range(0, len(alphas)):
-            alpha = alphas[n]
-            ps[n] = (y_vals >= alpha).sum() / 1000000
-        return ps
-
-    def markov(a): return ex / a
-    def cheb(a): return vx / (n * ((a - ex) ** 2))
-    def hoff(a): return 1 / (math.e ** (2 * n * ((a - ex) ** 2)))
-
-    alpha = np.arange(0.1, 1.05, 0.05)
-
-    freq_y = emfreq()
-    markov_y = np.array(list(map(markov, alpha)))
-    cheb_y = np.array(list(map(cheb, alpha)))
-    hoff_y = np.array(list(map(hoff, alpha)))
-
-    plt.plot(alpha, freq_y, label="Empirical frequency")
-    plt.plot(alpha, markov_y, label="Markov")
-    plt.plot(alpha, cheb_y, label="Chebyshev")
-    plt.plot(alpha, hoff_y, label="Hoeffding")
-    plt.legend()
-    plt.ylabel(r"$\mathbb{P}(\frac{1}{20}\sum^{20}_{i = 1}{X_i} \geq \alpha)$")
-    plt.xlabel("\u03B1")
-    plt.ylim(0, 1)
-    plt.xlim(0.1, 1)
-    plt.title("Bias of 0.1")
-    plt.savefig("./Plots/plot_q3_2", dpi=200)
 
 
 if __name__ == '__main__':
